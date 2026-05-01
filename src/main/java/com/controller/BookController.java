8a4c10d234b2f672a19ec21dba76cda376206e83
Mustafa9164 <mohdmustafa9164@gmail.com>
2026-05-01 12:27:36 +0530

    return String as return type

--- src/main/java/com/controller/BookController.java
+++ src/main/java/com/controller/BookController.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.entity.Book;
 import com.repo.BookRepo;
+
 
 @Controller
 public class BookController {
@@ -18,20 +20,17 @@ public class BookController {
 	private BookRepo bookRepo;
 	
 	@GetMapping("/book")
-	private ModelAndView getBookById(@RequestParam("id") Integer id) {
-		System.out.println("Id"+id);
-		ModelAndView mav =new ModelAndView();
+	private String getBookById(@RequestParam("id") Integer id, Model  model) {
 		
 		Optional<Book> findById = bookRepo.findById(id);
 		
 		if(findById.isPresent()) {
 			Book bookObj = findById.get();
-			mav.addObject("book", bookObj);
+			model.addAttribute("book", bookObj);
 		}
 		
-		mav.setViewName("index");
+		 return "index";
 		
-		return mav;
 	}
 	
 
